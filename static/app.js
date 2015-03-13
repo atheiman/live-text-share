db2aa80007a77f2afc7cbc634ed7ff2cd0bf769b
Austin Heiman <atheimanksu@gmail.com>
2015-03-13 15:03:45 -0500

    functionality working, still emitting text change on page load tho... nbd, deploying

--- static/app.js
+++ static/app.js
@@ -2,10 +2,10 @@ function App() {
   this.debug = function (msg) {
     var hostnames = ['localhost', 'github.com', '127.0.0.1'];
     if (hostnames.indexOf(location.hostname) !== -1)
-      console.log(msg);
+      console.log('DEBUG: ' + msg);
   };
 
-  this.textarea = function () {return document.getElementById('textarea');};
+  this.textarea = document.getElementById('textarea');
 
   this.getDateTimeStr = function () {
     var d = new Date();
@@ -27,31 +27,32 @@ function App() {
 
   this.textChange = function () {
     // emit the textChange to all connected users
-    socket.emit('text change', app.textarea().value);
-    app.debug('emit text change: ' + app.textarea().value);
+    socket.emit('text change', app.textarea.value);
+    app.debug('emit text change: ' + app.textarea.value);
   }
 
   this.updateTextarea = function (text) {
     // do nothing if received text is textarea value
-    if (app.textarea().value === text)
+    if (app.textarea.value === text)
       return;
 
-    app.debug('received text change: ' + app.textarea().value);
-    var pos = app.getDiffPos(app.textarea().value, text),
+    app.debug('received text change: ' + app.textarea.value);
+    var pos = app.getDiffPos(app.textarea.value, text),
         selectChange = 0,
-        selectStart = app.textarea().selectionStart,
-        selectEnd = app.textarea().selectionEnd;
+        selectStart = app.textarea.selectionStart,
+        selectEnd = app.textarea.selectionEnd;
 
-    if (pos < app.textarea().selectionStart) {
-      app.debug('selectionStart is after pos of change');
-      selectChange += text.length - app.textarea().length;
+    if (pos < app.textarea.selectionStart) {
+      app.debug('change is behind selectionStart');
+      selectChange += text.length - app.textarea.value.length;
+      app.debug('selectChange: ' + selectChange);
     }
 
     // update the value
-    app.textarea().value = text;
+    app.textarea.value = text;
 
     // set the selection change
-    app.textarea().selectionStart = selectStart + selectChange;
-    app.textarea().selectionEnd = selectEnd + selectChange;
+    app.textarea.selectionStart = selectStart + selectChange;
+    app.textarea.selectionEnd = selectEnd + selectChange;
   };
 }
